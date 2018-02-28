c406911d78969009aec09f8eae06ea5be49d3988
Slashcash <gen.iorio92@gmail.com>
2018-02-28 20:08:33 +0100

    Improve log writing

--- include/logwriter.hpp
+++ include/logwriter.hpp
@@ -9,17 +9,31 @@
 
 #include "result.hpp"
 
-/*Inherit from this class if you want to have a consistent log writing experience with your class*/
+/*Inherit (virtually?) from this class if you want to have a consistent log writing experience with your class*/
 
 namespace fs = std::experimental::filesystem::v1; //we have a filesystem class but we don't use it as this class is really particular, we prefer to manage everything in class
 
 class LogWriter {
+    public:
+        //enum
+        enum Error {
+            ERR_OPEN_LOGFILE = 1
+        };
+
+        //you can write different things on the log, and you can exclude or include log messages of certain types
+        enum Type {
+            MSG,
+            WARNING,
+            ERROR
+        };
+
     private:
         //members
         static std::mutex log_mutex; //this mutex protects the shared stream resources (as ideally this class has to be used even in different threads)
         static std::ostream& console_log; //a reference to the console log ( usually clog :D )
         static std::ofstream file_log; //if the log gets redirected to a file we store the file stream here
         static bool redirect_to_file; //this helps us knowing if the stream has been redirected to a file
+        static Type filter_type; //set this variable to something different to set the message filtering by type
 
     protected:
         //ctor
@@ -28,13 +42,8 @@ class LogWriter {
         //static functions
         static Result redirectToFile(const fs::path& thePath); //use this if you want to redirect the stream to a file
         static void redirectToConsole() { redirect_to_file = false; } //redirects it back to the console
-        static void writeToLog(const std::string& theMsg);
-
-    public:
-        //enum
-        enum Error {
-            ERR_OPEN_LOGFILE = 1
-        };
+        static void writeToLog(const std::string& theMsg, const Type& theMsgType = MSG);
+        static void filterType(const Type& theType) { filter_type = theType; } //use this function to filter out the log, MSG will show all messages, WARNING will show warnings and errors, ERRORS will show only errors
 };
 
 #endif // _LOGWRITER_HPP_
--- src/logwriter.cpp
+++ src/logwriter.cpp
@@ -6,6 +6,7 @@ std::ostream& LogWriter::console_log = std::clog;
 std::ofstream LogWriter::file_log;
 bool LogWriter::redirect_to_file = false;
 std::mutex LogWriter::log_mutex;
+LogWriter::Type LogWriter::filter_type = LogWriter::Type::MSG;
 
 Result LogWriter::redirectToFile(const fs::path& thePath) {
     file_log.open(thePath.string().c_str(), std::ios::out);
@@ -14,9 +15,20 @@ Result LogWriter::redirectToFile(const fs::path& thePath) {
     else { redirect_to_file = true; return Result(Result::SUCCESS); }
 }
 
-void LogWriter::writeToLog(const std::string& theMsg) {
-    log_mutex.lock(); //remember that console_log and file_log are shared resources and we want this class to be thread safe when writing to a log
-    if( !redirect_to_file ) console_log << theMsg; //write it on console
-    else file_log << theMsg; //or on a file
-    log_mutex.unlock();
+void LogWriter::writeToLog(const std::string& theMsg, const Type& theMsgType) {
+    //see if the message has to be filtered out or not
+    bool to_show = false;
+    std::string to_write = theMsg + "\n"; //appending a newline character
+
+    if( filter_type == MSG ) to_show = true;
+    else if( filter_type == WARNING && (theMsgType == WARNING || theMsgType == ERROR) ) { to_show = true; to_write = "WARNING: " + theMsg; }
+    else if( filter_type == ERROR && theMsgType == ERROR ) { to_show = true; to_write = "ERROR: " + theMsg; }
+
+    //only writing if it is not filtered out
+    if( to_show ) {
+        log_mutex.lock(); //remember that console_log and file_log are shared resources and we want this class to be thread safe when writing to a log
+        if( !redirect_to_file ) console_log << to_write; //write it on console
+        else file_log << to_write; //or on a file
+        log_mutex.unlock();
+    }
 }
