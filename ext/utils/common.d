6cedfbeffdf2155a148e5bd3a06c50190549e1e6
Slashcash <gen.iorio92@gmail.com>
2018-03-12 16:17:58 +0100

    Add some file needed for wireless support

---
+++ ext/utils/common.d
@@ -0,0 +1,3 @@
+common.o: common.c includes.h build_config.h ../common/ieee802_11_defs.h \
+ ../utils/common.h ../utils/os.h ../utils/wpa_debug.h ../utils/wpabuf.h \
+ common.h
---
+++ ext/utils/os_unix.d
@@ -0,0 +1,2 @@
+os_unix.o: os_unix.c includes.h build_config.h os.h common.h wpa_debug.h \
+ wpabuf.h
