4f99b049472c4ade22251cf381702fe034da46d7
Gennaro Iorio <mail@geniorio.it>
2024-10-20 14:12:07 +0200

    refactor: improve RomMedia class

--- lib/include/rommedia.hpp
+++ lib/include/rommedia.hpp
@@ -9,12 +9,35 @@
  */
 struct RomMedia
 {
+    /**
+     * @brief The label for the screenshot path in the json file
+     *
+     */
     static constexpr std::string_view SCREENSHOT_JSON_FIELD = "screenshot";
 
+    /**
+     * @brief Get the path where to locate a rom screenshot. This game
+     * screenshot can be used anywhere to enhance the presentation in the gui
+     *
+     */
     std::optional<std::filesystem::path> screenshot;
 };
 
+/**
+ * @brief nlohmann::json helper function. It is used by the nlohmann::json
+ * library in order to automatically convert the RomMedia struct to a json.
+ *
+ * @param json the output json
+ * @param info the input RomMedia struct
+ */
 void to_json(nlohmann::json& json, const RomMedia& media);
+/**
+ * @brief nlohmann::json helper function. It is used by the nlohmann::json
+ * library in order to automatically obtain a RomMedia from a json.
+ *
+ * @param json the input json
+ * @param info the output RomMedia struct
+ */
 void from_json(const nlohmann::json& json, RomMedia& media);
 
 #endif // ROMMEDIA_HPP
--- lib/source/rommedia.cpp
+++ lib/source/rommedia.cpp
@@ -1,26 +1,17 @@
 #include "rommedia.hpp"
 
+#include "utils.hpp"
+
 void to_json(nlohmann::json& json, const RomMedia& media)
 {
     json.clear();
 
     // Setting screenshot
-    if (const auto& screenshot = media.screenshot; screenshot)
-    {
-        json[RomMedia::SCREENSHOT_JSON_FIELD] = *screenshot;
-    }
+    utils::addOptionalToJson(json, RomMedia::SCREENSHOT_JSON_FIELD, media.screenshot);
 }
 
 void from_json(const nlohmann::json& json, RomMedia& media)
 {
     // Finding screenshot
-    try
-    {
-        media.screenshot = json.at(RomMedia::SCREENSHOT_JSON_FIELD).get<std::string>();
-    }
-    catch ([[maybe_unused]] const nlohmann::json::exception& e)
-    {
-        // Since screenshot is optional we just reset the value to empty if it's not found
-        media.screenshot.reset();
-    }
+    media.screenshot = utils::getValueFromJson<std::filesystem::path>(json, RomMedia::SCREENSHOT_JSON_FIELD);
 }
