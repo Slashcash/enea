19114369fb5ef94191aab2ac488ed148d49c304f
Gennaro Iorio <mail@geniorio.it>
2024-12-15 23:27:12 +0100

    build: exit with an error if building without compiling dependencies is not possible
    Previous implementation of the build script was attempting to build dependencies if
    they were not available on our conan remote. This behaviour was risky as it may
    hide the fact that our conan remote may miss some dependencies

--- scripts/generate_appimage.sh
+++ scripts/generate_appimage.sh
@@ -4,7 +4,7 @@
 DEFAULT_SOURCE_DIR="$( cd "$(dirname "$0/..")" >/dev/null 2>&1 ; pwd -P )"
 DEFAULT_OUTPUT_DIR="$DEFAULT_SOURCE_DIR"
 DEFAULT_ENEA_ARCH="x86_64"
-DEFAULT_BUILD_DEPS="missing"
+DEFAULT_BUILD_DEPS="never"
 DEFAULT_GENERATE_UPDATE_INFO=false
 
 # temporary folders
